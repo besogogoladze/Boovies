2573236b6622791abe18b1ec7b68969960be6487
besogogoladze <Gogoladzebeso8@gmail.com>
2021-10-23 18:10:44 +0200

    Updates

---
+++ precache-manifest.3163f76ea8a70a23954639e6039d774e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec092a93d591e653d8bf2effb636630",
+    "url": "/Boovies/index.html"
+  },
+  {
+    "revision": "e27d08b3e0ff20395b49",
+    "url": "/Boovies/static/css/2.9b167a4e.chunk.css"
+  },
+  {
+    "revision": "e18d12eeb0508abbad4c",
+    "url": "/Boovies/static/css/main.091eb38f.chunk.css"
+  },
+  {
+    "revision": "e27d08b3e0ff20395b49",
+    "url": "/Boovies/static/js/2.0fdbb826.chunk.js"
+  },
+  {
+    "revision": "04650511d6c69265254aaa0c18f0d8de",
+    "url": "/Boovies/static/js/2.0fdbb826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b087693d5be68123f2c",
+    "url": "/Boovies/static/js/3.68bf328e.chunk.js"
+  },
+  {
+    "revision": "e18d12eeb0508abbad4c",
+    "url": "/Boovies/static/js/main.84f7ccdf.chunk.js"
+  },
+  {
+    "revision": "3229ced33c5eda96bc8e",
+    "url": "/Boovies/static/js/runtime-main.7f79e204.js"
+  },
+  {
+    "revision": "afab6a33579ef36d0cf86fbad8f45d6f",
+    "url": "/Boovies/static/media/Logo.afab6a33.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Boovies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Boovies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Boovies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Boovies/static/media/slick.f97e3bbf.svg"
+  }
+]);
